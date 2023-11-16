2a528c938b2df669ac67566a1d1bb70e276f7fb2
Klaudiusz <134642600+PendolinoVoyager@users.noreply.github.com>
2023-11-16 18:50:05 +0100

    Firs version

---
+++ settings.py
@@ -0,0 +1,45 @@
+SETTINGS = {
+    'REFRESH_RATE': 500,
+    'CELLS_TO_STAY_ALIVE': [3, 2],
+    'CELLS_TO_COME_TO_LIFE': [3],
+    'GRID_SIZE': 25,
+    'CELL_SIZE': 20,
+}
+'''
+UPDATE_SETTINGS has a lot of conditions and I could probably shorten it but the
+rules aren't changed very often, so it doesn't matter
+'''
+def UPDATE_SETTINGS(key, value):
+    global SETTINGS
+    if key == 'CELLS_TO_STAY_ALIVE' or key == 'CELLS_TO_COME_TO_LIFE':
+        try:
+            values = value.strip().split()
+            temp_list = []
+            for item in list(map(int, values)):
+                if 9 > item > -1:
+                    temp_list.append(item)
+                else:
+                    continue
+            SETTINGS[key] = temp_list
+        except:
+            return
+    else:
+        try:
+            value = int(value)
+            if value < 0:
+                return
+            if key == 'GRID_SIZE':
+                if value > 200:
+                    SETTINGS[key] = 200
+                while value * SETTINGS['CELL_SIZE'] > 630:
+                    SETTINGS['CELL_SIZE'] -= 5
+                SETTINGS[key] = value
+                return
+            if key == 'CELL_SIZE':
+                if value > 200:
+                    SETTINGS[key] = 200
+                while value * SETTINGS['GRID_SIZE'] > 630:
+                    SETTINGS['GRID_SIZE'] -= 1
+            SETTINGS[key] = value
+        except:
+            return
